70a58552b1ebe8f5bfd35e82ee60c8ad8972080f
matias11972-7741 <matias11972@gmail.com>
2025-12-27 09:54:46 -0300

    fix: Envolver el retorno de VideoPlayer en React.Fragment para corregir error de compilación
    Se ha envuelto el contenido JSX retornado por el componente VideoPlayer en un React.Fragment.
    Esto debería solucionar el error de compilación 'Unexpected token div. Expected jsx identifier' que ocurría durante el despliegue en Vercel, asegurando que el parser de JSX maneje correctamente el elemento raíz.

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -277,7 +277,8 @@ const VideoPlayer = forwardRef<VideoPlayerRef, VideoPlayerProps>(
     }), []);
 
     return (
-      <div className="relative w-full h-full">
+      <React.Fragment>
+        <div className="relative w-full h-full">
         {/* (Intro - sin cambios) */}
         <AnimatePresence>
           {showIntro && (
@@ -366,6 +367,7 @@ const VideoPlayer = forwardRef<VideoPlayerRef, VideoPlayerProps>(
         </div>
         <div className="absolute inset-0 z-10"></div>
       </div>
+      </React.Fragment>
     );
   }
 );
