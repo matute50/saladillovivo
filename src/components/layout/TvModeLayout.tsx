495fe136d9c26df51ddbc7c5daf17a1e26c9db6a
matias11972-7741 <matias11972@gmail.com>
2026-01-09 09:44:45 -0300

    fix: Probar con vercel.svg para depurar el logo roto
    - Cambia temporalmente la imagen del logo de FONDO OSCURO.PNG a ercel.svg en TvModeLayout.tsx para aislar la causa del problema del logo roto en producción.

--- src/components/layout/TvModeLayout.tsx
+++ src/components/layout/TvModeLayout.tsx
@@ -304,23 +304,23 @@ const TvModeLayout = () => {
 
                                 >
 
-                                                                                                      <Image
+                                                                                                                                        <Image
 
-                                                                                                        src="/FONDO OSCURO.PNG"
+                                                                                                                                          src="/vercel.svg"
 
-                                                                                                        alt="Saladillo Vivo Logo" 
+                                                                                                                                          alt="Saladillo Vivo Logo" 
 
-                                                                                          width={192} 
+                                                                                                                            width={192} 
 
-                                                                                          height={48} 
+                                                                                                                            height={48} 
 
-                                                                                          className="h-auto w-48 object-contain"
+                                                                                                                            className="h-auto w-48 object-contain"
 
-                                                                                          priority
+                                                                                                                            priority
 
-                                                                                          unoptimized={true}
+                                                                                                                            unoptimized={true}
 
-                                                                                        />
+                                                                                                                          />
 
                   </div>
 
